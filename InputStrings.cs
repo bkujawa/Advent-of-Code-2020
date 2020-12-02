d495182c507009ebc79ed9cd0e0b30eaffbeeb63
Bartosz Kujawa <bkujawa@gmail.com>
2020-12-02 16:47:16 +0100

    Day 1

---
+++ InputStrings.cs
@@ -0,0 +1,9 @@
+namespace Advent_of_Code_2020
+{
+    public static class InputStrings 
+    {
+        public static string InputStringDay1 = "InputDay1.txt";
+        public static string InputStringDay2 = "InputDay2.txt";
+        public static string InputStringDay3 = "InputDay3.txt";
+    }
+}
